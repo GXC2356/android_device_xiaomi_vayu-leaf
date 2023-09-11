1177712b02499da3899cefab8404fb1979f49eee
GXC2356 <108477326+GXC2356@users.noreply.github.com>
2023-09-11 21:16:11 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vayu.mk
+    $(LOCAL_DIR)/vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vayu-user \
-    lineage_vayu-userdebug \
-    lineage_vayu-eng
+    vayu-user \
+    vayu-userdebug \
+    vayu-eng
